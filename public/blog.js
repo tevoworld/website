48c60c4e2392a30ee7c89fef6f7317f15e6f1672
televisionia <106242960+televisionia@users.noreply.github.com>
2024-05-04 14:44:15 +1000

    Added the blog page.

---
+++ public/blog.js
@@ -0,0 +1,40 @@
+var dir = "/blog/"
+
+
+function load_post(post) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', post, true);
+    xhr.onreadystatechange= function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return; // or whatever error handling you want
+        document.getElementById('post_box').innerHTML= this.responseText;
+    };
+    xhr.send();
+};
+
+function load_list() {
+    var list_box = document.getElementById("post_list");
+
+    $.ajax({url: dir}).then(function(html) {
+        var ndocument = $(html);
+
+        ndocument.find('a[href$=".html"]').each(function() {
+            var Name = $(this).attr('title').slice(0, $(this).attr('title').lastIndexOf('.'));
+            var Url = $(this).attr('href');
+            
+            var new_div = document.createElement('div');
+            new_div.className = 'blog_list_entry';
+            list_box.appendChild(new_div);
+
+            var new_title = document.createElement('h4');
+            new_title.innerHTML = Name;
+            new_div.appendChild(new_title);
+
+            new_div.addEventListener('click', function (event) {
+                load_post(Url);
+            });
+        });
+    });
+     
+};
+load_list();
