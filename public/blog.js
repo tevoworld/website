229cbd1dec6ac3d7a768248e4568f7aeb1e62f60
televisionia <106242960+televisionia@users.noreply.github.com>
2024-09-07 12:18:22 +1000

    Attempt at fixing blog for neocities

--- public/blog.js
+++ public/blog.js
@@ -1,6 +1,10 @@
 var dir = "/blog/"
 
 
+/* Resort to using arrays next time. */
+
+var pages = ["info", "Test Post"]
+
 function load_post(post) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', post, true);
@@ -13,32 +17,23 @@ function load_post(post) {
 };
 
 function load_list() {
-    console.log("Loading list...")
     var list_box = document.getElementById("post_list");
-    console.log(list_box)
 
-    $.ajax({url: dir}).then(function(html) {
-        var ndocument = $(html);
-        console.log(ndocument)
+    for (page_name in pages) {
+        var page = dir + page_name + ".html"
 
-        ndocument.find('a[href$=".html"]').each(function() {
-            var Name = $(this).attr('title').slice(0, $(this).attr('title').lastIndexOf('.'));
-            console.log(Name)
-            var Url = $(this).attr('href');
-            
-            var new_div = document.createElement('div');
-            new_div.className = 'blog_list_entry';
-            list_box.appendChild(new_div);
+        var new_div = document.createElement('div');
+        new_div.className = 'blog_list_entry';
+        list_box.appendChild(new_div);
 
-            var new_title = document.createElement('h4');
-            new_title.innerHTML = Name;
-            new_div.appendChild(new_title);
+        var new_title = document.createElement('h4');
+        new_title.innerHTML = page_name;
+        new_div.appendChild(new_title);
 
-            new_div.addEventListener('click', function (event) {
-                load_post(Url);
-            });
+        new_div.addEventListener('click', function (event) {
+        load_post(page);
         });
-    });
+    };
      
 };
 load_list();
