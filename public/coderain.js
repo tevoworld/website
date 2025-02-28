d85006ede46f3b5ee3be44c61aee90a0c28ca73f
televisionia <106242960+televisionia@users.noreply.github.com>
2025-02-28 23:02:57 +1100

    Ran prettify code formatter on the code files.

--- public/coderain.js
+++ public/coderain.js
@@ -1,21 +1,21 @@
-const canvas = document.getElementById('rain_canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("rain_canvas");
+const ctx = canvas.getContext("2d");
 
-const w = canvas.width = document.body.offsetWidth;
-const h = canvas.height = document.body.offsetHeight;
+const w = (canvas.width = document.body.offsetWidth);
+const h = (canvas.height = document.body.offsetHeight);
 const cols = Math.floor(w / 20) + 1;
 const ypos = Array(cols).fill(0);
 
-ctx.fillStyle = '#000';
+ctx.fillStyle = "#000";
 ctx.fillRect(0, 0, w, h);
 
-function matrix () {
+function matrix() {
   ctx.fillStyle = canvas.dataset.colorfill;
   ctx.fillRect(0, 0, w, h);
-  
+
   ctx.fillStyle = canvas.dataset.color;
-  ctx.font = '15pt Silkscreen';
-  
+  ctx.font = "15pt Silkscreen";
+
   ypos.forEach((y, ind) => {
     const text = String.fromCharCode(Math.random() * 128);
     const x = ind * 20;
--- public/global.js
+++ public/global.js
@@ -1,62 +1,62 @@
 function redirect(url) {
-    window.location.href = url;
-};
+  window.location.href = url;
+}
 
 localStorage.currentpage = window.location.href;
 
-
 if (localStorage.name == "") {
-    redirect("index.html");
-};
+  redirect("index.html");
+}
 
-window.addEventListener('load', function () {
-    var names = document.getElementsByClassName('name');
+window.addEventListener("load", function () {
+  var names = document.getElementsByClassName("name");
 
-    for (var i = 0; i < names.length; ++i) {
-        var text = names[i];  
-        text.innerHTML = localStorage.name;
-    };
+  for (var i = 0; i < names.length; ++i) {
+    var text = names[i];
+    text.innerHTML = localStorage.name;
+  }
 
-    function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  function dragElement(elmnt) {
+    var pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
     if (document.getElementById(elmnt.id + "header")) {
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
     } else {
-        elmnt.onmousedown = dragMouseDown;
+      elmnt.onmousedown = dragMouseDown;
     }
 
     function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
     }
 
     function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
     }
 
     function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
+      document.onmouseup = null;
+      document.onmousemove = null;
     }
-    }
+  }
 
-    boxes = document.getElementsByClassName("draggable_box")
+  boxes = document.getElementsByClassName("draggable_box");
 
-    for (let i in boxes) {
-        dragElement(boxes[i]);
-        console.log(boxes[i]);
-    }
-
+  for (let i in boxes) {
+    dragElement(boxes[i]);
+    console.log(boxes[i]);
+  }
 });
-
